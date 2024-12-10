59ed5a4baf5512ed4be717e33360e41ce6540441
unknown <buiduykhanh0162003@gmail.com>
2024-12-10 14:12:12 +0700

    first commit

---
+++ src/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,10 @@
+package fizzbuzz;
+
+import static fizzbuzz.FizzBuzz.fizzBuzz;
+
+public class FizzBuzzTest {
+    public static void main(String[] args) {
+        String result = (fizzBuzz(5));
+        System.out.println(result);
+    }
+}
